8e8e1e79175079e79128e3d36d9bb6e47c86bf57
Aleksey Zhukov <jhukov2001@mail.ru>
2025-03-01 16:43:40 +0300

    Initial commit

---
+++ src/main/kotlin/ControlStructures.kt
@@ -0,0 +1,41 @@
+fun main() {
+    println(agoToText((60 * 60) * 16))
+}
+
+fun agoToText(seconds: Int): String {
+    when (seconds) {
+        in 0..60 -> return "был(а) только что"
+        in 61..(60 * 60) -> return "был(а) " + minutes(seconds)
+        in ((60 * 60) + 1)..((60 * 60) * 24) -> return "был(а) " + hours(seconds)
+        in ((60 * 60) * 24)..((60 * 60) * 48) -> return "был(а) вчера"
+        in ((60 * 60) * 48)..((60 * 60) * 72) -> return "был(а) позавчера"
+        else -> return "был(а) давно"
+    }
+}
+
+fun minutes(seconds: Int): String {
+    var minutes = seconds / 60
+    if (minutes == 11 || minutes == 12 || minutes == 13 || minutes == 14 || minutes == 15 || minutes == 16 || minutes == 17 || minutes == 18 || minutes == 19) {
+        return "$minutes минут назад"
+    } else if (minutes % 10 == 1) {
+        return "$minutes минуту назад"
+    } else if (minutes % 10 == 2 || minutes % 10 == 3 || minutes % 10 == 4) {
+        return "$minutes минуты назад"
+    } else if (minutes == 60) {
+        return "час назад"
+    } else if (minutes % 10 == 5 || minutes % 10 == 6 || minutes % 10 == 7 || minutes % 10 == 8 || minutes % 10 == 9 || minutes % 10 == 0) {
+        return "$minutes минут назад"
+    }
+    return ""
+}
+
+fun hours(seconds: Int): String {
+    var hours = seconds / (60 * 60)
+    if (hours == 1 || hours == 21) {
+        return "$hours час назад"
+    }else if (hours == 2 || hours == 3 || hours == 4 || hours == 22 || hours == 23 || hours == 24) {
+        return "$hours часа назад"
+    }else{
+        return "$hours часов назад"
+    }
+}

642a4ce2a6d93e76f4dbaa9d5d7892adc1f5109b
Aleksey Zhukov <jhukov2001@mail.ru>
2025-03-01 20:58:11 +0300

    Thread commit

--- src/main/kotlin/ControlStructures.kt
+++ src/main/kotlin/ControlStructures.kt
@@ -1,5 +1,5 @@
 fun main() {
-    println(agoToText((60 * 60) * 2))
+    println(agoToText(60 + 1))
 }
 
 fun agoToText(seconds: Int): String {
@@ -15,25 +15,39 @@ fun agoToText(seconds: Int): String {
 
 fun minutes(seconds: Int): String {
     var minutes = seconds / 60
-    if (minutes == 11 || minutes == 12 || minutes == 13 || minutes == 14 || minutes == 15 || minutes == 16 || minutes == 17 || minutes == 18 || minutes == 19) {
-        return "$minutes минут назад"
-    } else if (minutes % 10 == 1) {
-        return "$minutes минуту назад"
-    } else if (minutes % 10 == 2 || minutes % 10 == 3 || minutes % 10 == 4) {
-        return "$minutes минуты назад"
-    } else if (minutes == 60) {
-        return "час назад"
-    } else if (minutes % 10 == 5 || minutes % 10 == 6 || minutes % 10 == 7 || minutes % 10 == 8 || minutes % 10 == 9 || minutes % 10 == 0) {
-        return "$minutes минут назад"
+
+    return when {
+        minutes == 11 || minutes == 12 || minutes == 13 || minutes == 14 || minutes == 15 || minutes == 16 || minutes == 17 || minutes == 18 || minutes == 19 -> {
+            "$minutes минут назад"
+        }
+
+        minutes % 10 == 1 -> {
+            "$minutes минуту назад"
+        }
+
+        minutes % 10 == 2 || minutes % 10 == 3 || minutes % 10 == 4 -> {
+            "$minutes минуты назад"
+        }
+
+        minutes == 60 -> {
+            "час назад"
+        }
+
+        minutes % 10 == 5 || minutes % 10 == 6 || minutes % 10 == 7 || minutes % 10 == 8 || minutes % 10 == 9 || minutes % 10 == 0 -> {
+            "$minutes минут назад"
+        }
+
+        else -> {
+            ""
+        }
     }
-    return ""
 }
 
 fun hours(seconds: Int): String {
     var hours = seconds / (60 * 60)
-    when (hours) {
-        1, 21 -> return "$hours час назад"
-        2,3,4,22,23,24 -> return "$hours часа назад"
-        else -> return "$hours часов назад"
+    return when (hours) {
+        1, 21 -> "$hours час назад"
+        2, 3, 4, 22, 23, 24 -> "$hours часа назад"
+        else -> "$hours часов назад"
     }
 }
